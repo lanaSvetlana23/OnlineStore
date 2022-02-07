70e71744cb12716b9255db786839e19290b1719c
SvetaDudko <kss.dsg.2011@gmail.com>
2022-02-07 14:41:06 +0100

    add items to basket

--- basket.js
+++ basket.js
@@ -17,7 +17,26 @@ function addToCard(event) {
     let buttonId = event.target.id;
     let infoId = "icon_info_" + buttonId.replace('btn_', '');
     let iconInfo = document.querySelector("#" + infoId);
-    console.log(iconInfo);
+    let iconChildren = iconInfo.children;
+    let iconChildrenH3 = iconChildren[0].children[0].innerHTML;
+    let iconChildrenStrong = iconChildren[2].innerHTML;
+    let productCount = document.querySelector('.productCount' + infoId);
+    if (productCount) {
+        productCount.innerHTML = Number(productCount.innerHTML) + 1;
+    } else {
+        let basketItem = `<div class="basketRow">
+                            <div>${iconChildrenH3}</div>
+                            <div>
+                                <span class="productCount${infoId}">1</span> шт.
+                            </div>
+                            <div>${iconChildrenStrong}</div>
+                            <div>
+                                <span class="productTotalRow" data-total_productId="${infoId}">100500</span>
+                            </div>
+                        </div>`;
+        let addDiv = document.querySelector('.addDiv');
+        addDiv.innerHTML += basketItem;
+    }
 }
 
 
